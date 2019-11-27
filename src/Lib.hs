898006e4473c86c32d30c82abe31573d1d3c741b
Adam Massmann <akm2203@columbia.edu>
2019-11-27 16:10:08 -0500

    some progress on laying out

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,36 +5,73 @@ module Lib
 import Prelude
 import Data.Propagator
 import GHC.Generics (Generic)
+import Control.Monad (replicateM)
 import Control.Monad.ST
+import Control.Monad.Primitive
+import System.Random.MWC (create, GenST)
+import Statistics.Distribution
+import Statistics.Distribution.Normal
 
-type Step = Int
+newtype Time = T Int deriving (Show, Eq, Ord, Read)
+
 type S = Double
+type LogLikelihood = Double
 
-newtype VariationalProp = V (Step, S, Distribution) deriving (Show, Read, Eq, Generic)
+type QDistribution = NormalDistribution
+data QProp s = Q { gradMemory :: !S
+                 , distribution :: !QDistribution
+                 , generator :: !(GenST s)
+                 , loglike :: !Double
+                 }
 
-newtype Distribution = D NormalDistribution deriving (Show, Read, Eq, Generic)
+class VariationalLogic a where
+  difference :: a -> a -> Double
 
-data NormalDistribution = ND
-  { mean :: !Double
-  , std :: !Double
-  , variance :: !Double
-  } deriving (Show, Read, Eq, Generic)
+  logProb :: a -> Double -> Double
 
-normalDistr :: Double -> Double -> NormalDistribution
-normalDistr m s = ND m s (s * s)
 
-instance Propagated VariationalProp where
-  merge (V (i1, s1, d1)) (V (i2, s2, d2))
-    | i1 >= i2 = Change False (V (i2, s1, d1))
-    | otherwise = Change True (V (i2, s2, d2))
+instance VariationalLogic NormalDistribution where
+  difference x1 x2 = sqrt (f mean + f stdDev)
+    where
+      f g = (g x1 - g x2) ^ 2
+
+  logProb = logDensity
+
+instance Propagated (QProp s) where
+  merge q1 q2
+    | f q1 q2 < 0.01 = Change False q1
+    | loglike q2 == 0 = Change True q2
+    | otherwise = Change False (q1 {loglike = (loglike q1 + loglike q2)})
+    where
+      f x1 x2 = difference (distribution x1) (distribution x2)
+
+maxStep :: Time
+maxStep = T 20000
+
+instance Propagated Time where
+  merge t1 t2
+    | t1 > maxStep = Change False t1
+    | t2 > t1 = Change True t2
+    | otherwise = Change False t1
+
+normalProp std prior xs t q = do
+  watch q $ \qprop ->
+    write q (qprop {loglike = sum (fmap (logProb (distribution qprop)) xs)}) >>
+    with t (\(T t') -> write t (T (t' + 1)))
+
+updateQ t q = 0.0  -- this is todo, make sure to zero log likelihood and calcualted needed things.
 
 -- propagator :: () -- Maybe VariationalProp
-propagator = runST $ do
-  x <- known $ V (0, 0.0, D (normalDistr 0.0 1.0))
-  lift1 (\(V (i, s, d)) -> if i == 5 then V (i, s, d) else V (i + 1, s, d)) x x
-  content x
+propagator xs = runST $ do
+  gen <- create
+  let prior = normalDistr 0.0 2.0
+  q <- known $ Q 0.0 (normalDistr 0.0 2.0) gen 0.0
+  (distribution <$>) <$> content q
 
 someFunc :: IO ()
-someFunc = putStrLn (show propagator)
+someFunc = do
+  gen <- create
+  xs <- replicateM 1000 (genContinuous (normalDistr 5.0 3.0) gen)
+  putStrLn (show $ propagator xs)
 -- >>> someFunc
--- Just (V (5,0.0,D (ND {mean = 0.0, std = 1.0, variance = 1.0})))
+-- Just (normalDistr 0.0 1.0)
