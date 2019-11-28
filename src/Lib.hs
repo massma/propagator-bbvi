411bbbd8890a838490755371eacd848d89ad27e8
Adam Massmann <akm2203@columbia.edu>
2019-11-28 15:33:38 -0500

    tuning for eta and change in q makes a difference

--- src/Lib.hs
+++ src/Lib.hs
@@ -65,7 +65,7 @@ instance VariationalLogic NormalDistribution where
 -- see https://stats.stackexchange.com/questions/404191/what-is-the-log-of-the-pdf-for-a-normal-distribution
 instance Propagated (QProp s) where
   merge q1 q2
-    -- | f q1 q2 < 0.01 = Change False q1
+    | f q1 q2 < 0.0001 = Change False q1
     | otherwise = Change True q2
     where
       f x1 x2 = difference (distribution x1) (distribution x2)
@@ -77,26 +77,14 @@ instance Propagated LogLikelihood where
     | otherwise = Change True (V.zipWith (+) l1 l2)
 
 maxStep :: Time
-maxStep = T 5
+maxStep = T 1000000
 
+-- | time can only increase in increments of 1. syntax is to write t
+-- to 1, b/c that way an empty t starts at 1
 instance Propagated Time where
   merge t1 t2
-    | t1 > maxStep = Change False t1
-    | t2 > t1 = Change True t2
-    | otherwise = Change False t1
-
--- | don't forget prior with variational distribution - thin kmaybe we
--- should icnorporate prior into QProp and use it when we update q
-normalProp prior std xs t q l = do
-  watch q $ \qprop ->
-    write
-      l
-      (fmap
-         (\mu ->
-            logDensity prior mu + -- TODO: move prior updateQ
-            (sum $ fmap (logDensity (normalDistr mu std)) xs))
-         (samples qprop)) >>
-    with t (\(T t') -> write t (T (t' + 1)))
+    | t1 >= maxStep = Change False t1
+    | otherwise = Change True (T (time t1 + 1))
 
 gradient dist like samples = V.map (/ (fromIntegral $ V.length summed)) summed
   where
@@ -126,7 +114,7 @@ updateQ nSamp Q{..} t l =
   Q s' newDist generator <$> V.replicateM nSamp (genContinuous newDist generator)
   where
     alpha = 0.1 -- from kuckelbier et al
-    eta = 0.01 -- coservative, from kuckelbiet et al
+    eta = 0.1 -- 1 -- 10 -- 100 -- 0.01 -- this needs tuning
     tau = 1.0
     epsilon = 1e-16
     grad = gradient distribution l samples
@@ -140,36 +128,38 @@ updatePropQ nSamp t q l =
       with l $ \l' ->
         updateQ nSamp q' t' l' >>= \newq -> write l V.empty >> write q newq
 
+-- | don't forget prior with variational distribution - thin kmaybe we
+-- should icnorporate prior into QProp and use it when we update q
+-- TODO: consider setting explicit minimum on stddev
+normalProp prior std xs t q l = do
+  watch q $ \qprop ->
+    write
+      l
+      (fmap
+         (\mu ->
+            logDensity prior mu + -- TODO: move prior updateQ
+            (sum $ fmap (logDensity (normalDistr mu std)) xs))
+         (samples qprop)) >>
+    write t (T 1)
+
+
 -- how to efficiently watch all of our t's????
 
 -- propagator :: () -- Maybe VariationalProp
 propagator xs = runST $ do
   gen <- create
   let prior = normalDistr 0.0 2.0
-  q <- known $ Q (V.fromList [0.0, 0.0]) (normalDistr 0.0 2.0) gen V.empty
-  t <- known $ T 1
+  let nSamp = 100
+  let qDist = (normalDistr 0.0 2.0)
+  initSamp <- V.replicateM nSamp (genContinuous qDist gen)
+  q <- known $ Q (V.fromList [0.0, 0.0]) qDist gen initSamp
+  t <- cell
   l <- known $ V.empty
-  updatePropQ 10 t q l
+  updatePropQ nSamp t q l
   normalProp prior 1.0 xs t q l
   q' <- content q
   t' <- content t
-  return (distribution <$> q', time <$> t', samples <$> q')
-
-newtype Test = Test Int deriving (Eq, Ord, Read, Show)
-
-instance Propagated Test where
-  merge t1 t2
-    | t2 >= (Test 30) = Change False t2
-    | t1 >= (Test 5) = Change False t2
-    | otherwise = Change True t2
-
-testProp = runST $ do
-  x <- known $ Test 0
-  lift1 (\(Test x) -> Test 100) x x
-  content x
--- >>> testProp
--- Just (Test 0)
-
+  return (distribution <$> q', time <$> t')
 
 someFunc :: IO ()
 someFunc = do
@@ -177,4 +167,4 @@ someFunc = do
   xs <- replicateM 1000 (genContinuous (normalDistr 5.0 3.0) gen)
   putStrLn (show $ propagator xs)
 -- >>> someFunc
--- (Just (normalDistr 4.698429664574176e-2 1.9510165133417525),Just 6,Just [-4.175372991091955,-0.6927839199544554,-0.7017981683893668,-1.560056375926866,-0.4274962807947022,-2.006133086056551,2.655967899924271,-0.7725469560239188,0.22084589204472316,-1.5117093518251352])
+--
