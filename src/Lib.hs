115117b4b3efe20d1c65046955a867152e61713d
Adam Massmann <akm2203@columbia.edu>
2019-11-28 11:57:26 -0500

    fixed, now we just need to figure out how to intialize w/ termination condition

--- src/Lib.hs
+++ src/Lib.hs
@@ -21,15 +21,13 @@ newtype Time = T Int deriving (Show, Eq, Ord, Read)
 time (T t) = t
 
 type S = (V.Vector Double)
-type LogLikelihood = Double
-
+type LogLikelihood = (V.Vector Double)
 
 -- | Q: should we store prior with QProp? - then would have to pass xs to updateQ
 type QDistribution = NormalDistribution
 data QProp s = Q { gradMemory :: !S
                  , distribution :: !QDistribution
                  , generator :: !(GenST s)
-                 , loglike :: !(V.Vector Double)
                  , samples :: !(V.Vector Double)
                  }
 
@@ -67,15 +65,19 @@ instance VariationalLogic NormalDistribution where
 -- see https://stats.stackexchange.com/questions/404191/what-is-the-log-of-the-pdf-for-a-normal-distribution
 instance Propagated (QProp s) where
   merge q1 q2
---    | f q1 q2 < 0.01 = Change False q1
-    | null (loglike q2) = Change True q2
-    | null (loglike q1) = Change False (q1 {loglike = loglike q2})
-    | otherwise = Change False (q1 {loglike = (V.zipWith (+) (loglike q1) (loglike q2))})
+    -- | f q1 q2 < 0.01 = Change False q1
+    | otherwise = Change True q2
     where
       f x1 x2 = difference (distribution x1) (distribution x2)
 
+instance Propagated LogLikelihood where
+  merge l1 l2
+    | null l2 = Change True l2
+    | null l1 = Change True l2
+    | otherwise = Change True (V.zipWith (+) l1 l2)
+
 maxStep :: Time
-maxStep = T 10
+maxStep = T 5
 
 instance Propagated Time where
   merge t1 t2
@@ -83,19 +85,17 @@ instance Propagated Time where
     | t2 > t1 = Change True t2
     | otherwise = Change False t1
 
--- | don't forget prior with variational distribution
-normalProp prior std xs t q = do
+-- | don't forget prior with variational distribution - thin kmaybe we
+-- should icnorporate prior into QProp and use it when we update q
+normalProp prior std xs t q l = do
   watch q $ \qprop ->
     write
-      q
-      (qprop
-         { loglike =
-             fmap
-               (\mu ->
-                  logDensity prior mu +
-                  (sum $ fmap (logDensity (normalDistr mu std)) xs))
-               (samples qprop)
-         }) >>
+      l
+      (fmap
+         (\mu ->
+            logDensity prior mu + -- TODO: move prior updateQ
+            (sum $ fmap (logDensity (normalDistr mu std)) xs))
+         (samples qprop)) >>
     with t (\(T t') -> write t (T (t' + 1)))
 
 gradient dist like samples = V.map (/ (fromIntegral $ V.length summed)) summed
@@ -122,22 +122,23 @@ rho alpha eta tau epsilon t grad s0 =
 -- ([0.0,0.0],[1.0e-2,1.0e-2])
 
 
-updateQ nSamp Q{..} t' =
-  Q s' newDist generator V.empty <$> V.replicateM nSamp (genContinuous newDist generator)
+updateQ nSamp Q{..} t l =
+  Q s' newDist generator <$> V.replicateM nSamp (genContinuous newDist generator)
   where
     alpha = 0.1 -- from kuckelbier et al
     eta = 0.01 -- coservative, from kuckelbiet et al
     tau = 1.0
     epsilon = 1e-16
-    grad = gradient distribution loglike samples
-    (s', rho') = rho alpha eta tau epsilon t' grad gradMemory
+    grad = gradient distribution l samples
+    (s', rho') = rho alpha eta tau epsilon t grad gradMemory
     newDist = fromParamVector $ V.zipWith3 (\x g r -> x + r*g) (paramVector distribution) grad rho'
 -- >>> create >>= \gen -> updateQ 10 (Q (V.fromList [0.0, 0.0]) (normalDistr 0.0 2.0) gen V.empty V.empty) 1
 
-updatePropQ nSamp t q =
+updatePropQ nSamp t q l =
   watch t $ \(T t') ->
-    -- with q $ \qprop -> write q qprop
-    with q $ \qprop -> updateQ nSamp qprop t' >>= \newq -> write q newq
+    with q $ \q' ->
+      with l $ \l' ->
+        updateQ nSamp q' t' l' >>= \newq -> write l V.empty >> write q newq
 
 -- how to efficiently watch all of our t's????
 
@@ -145,13 +146,14 @@ updatePropQ nSamp t q =
 propagator xs = runST $ do
   gen <- create
   let prior = normalDistr 0.0 2.0
-  q <- known $ Q (V.fromList [0.0, 0.0]) (normalDistr 0.0 2.0) gen V.empty V.empty
+  q <- known $ Q (V.fromList [0.0, 0.0]) (normalDistr 0.0 2.0) gen V.empty
   t <- known $ T 1
-  updatePropQ 10 t q
-  normalProp prior 1.0 xs t q
+  l <- known $ V.empty
+  updatePropQ 10 t q l
+  normalProp prior 1.0 xs t q l
   q' <- content q
   t' <- content t
-  return (distribution <$> q', time <$> t', samples <$> q', loglike <$> q')
+  return (distribution <$> q', time <$> t', samples <$> q')
 
 newtype Test = Test Int deriving (Eq, Ord, Read, Show)
 
@@ -175,4 +177,4 @@ someFunc = do
   xs <- replicateM 1000 (genContinuous (normalDistr 5.0 3.0) gen)
   putStrLn (show $ propagator xs)
 -- >>> someFunc
--- <interactive>:1492:2-9: error: Variable not in scope: someFunc
+-- (Just (normalDistr 4.698429664574176e-2 1.9510165133417525),Just 6,Just [-4.175372991091955,-0.6927839199544554,-0.7017981683893668,-1.560056375926866,-0.4274962807947022,-2.006133086056551,2.655967899924271,-0.7725469560239188,0.22084589204472316,-1.5117093518251352])
