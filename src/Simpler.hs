388088dad556e4a5827402e94158f530bbd37ffd
Adam Massmann <akm2203@columbia.edu>
2019-12-07 14:09:27 -0500

    fixed some bugs in dirichlet code, now testing mixed case again
    also split off old fucntions into test

--- src/Simpler.hs
+++ src/Simpler.hs
@@ -2,6 +2,28 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 module Simpler
   ( someFunc
+  , Differentiable(..)
+  , Dist(..)
+  , Sampleable(..)
+  , Dirichlet(..)
+  , defaultDirichlet
+  , dirichlet
+  , NormalDist(..)
+  , defaultNormalDist
+  , normalDistr
+  , mean
+  , stdDev
+  , Obs(..)
+  , defaultObs
+  , PropNode(..)
+  , PropNodes
+  , gradientScore
+  , gradientReparam
+  , unsafeContent
+
+  -- * probably ones we want to remove
+  , globalMaxStep
+  , globalDelta
   ) where
 import Prelude
 import Data.Propagator
@@ -47,13 +69,13 @@ normVec = sqrt . V.sum . V.map (^ (2 :: Int))
 type Time = Int
 
 globalMaxStep :: Time
-globalMaxStep = 1000000
+globalMaxStep = 100000
 
 globalDelta :: Double
 globalDelta = 1e-16
 
 globalEta :: Double
-globalEta =  0.1 --10.0
+globalEta =  0.01 --10.0
 
 type Samples = V.Vector
 
@@ -282,71 +304,6 @@ data KucP = KucP
 -- | eta is what you probably want to tune: kucukelbir trys 0.01 0.1 1 10 100
 defaultKucP = KucP {alpha = 0.1, eta = globalEta, tau = 1.0, eps = 1e-16}
 
--- normalLike2 nSamp std gen (xs, q) = do
---   samples <- V.replicateM nSamp (resample q gen)
---   obs <- V.replicateM nSamp (resample xs gen)
---   let like = V.map
---                (\eps ->
---                   V.sum $ V.map (logProb (normalDistr (transform q eps) std)) obs)
---                samples
---   return
---     ( U (O V.empty, O V.empty)
---     , gradientScore q (fromIntegral nObs, like, samples))
-
--- | specialize all fmaps to vector???
--- CAREFUL: with rao-blackweixation and my wieghting approach, all terms
--- in log likelihood functions must contain all nodes q
-normalLikeAD nSamp std gen xs q =
-  V.replicateM nSamp (epsilon q gen) >>= \samples ->
-    return
-      ( fromIntegral $ V.length xs
-      , V.map
-          (\eps ->
-             V.sum $
-             V.map (mean . paramGradOfLogQ (normalDistr (transform q eps) std)) xs)
-          samples
-      , samples)
-
-normalLikeAD2 nSamp std gen (xsN, qN) = do
-  obs <- V.replicateM nSamp (resample xs gen)
-  samples <- V.replicateM nSamp (epsilon q gen)
-  let gradLikes =
-        V.map
-          (\eps ->
-             V.sum $
-             V.map
-               (mean . paramGradOfLogQ (normalDistr (transform q eps) std))
-               (obs :: V.Vector Double))
-          samples
-  return $ gradientReparam qN ((fromIntegral nSamp), gradLikes, samples)
-  where
-    xs = dist xsN
-    q = dist qN
-
-normalFit xs =
-  runST $ do
-    genG <- create
-    gen1 <- initialize =<< V.replicateM 256 (uniform genG)
-    let nSamp = 100
-    xProp <- known $ defaultObs xs
-    q <-
-      known $
-      (defaultNormalDist
-         { prior = normalDistr 0.0 2.0
-         , dist = normalDistr 0.0 2.0
-         , weight = fromIntegral nSamp
-         })
-    (\qP xP ->
-       watch qP $ \q' ->
-         with xP $ \xs' ->
-           normalLikeAD2 nSamp 1.0 gen1 (xs', q') >>= write q)
-      q
-      xProp
-    -- qPropAD2 (normalLikeAD2 nSamp nSamp 1.0 gen1) (xProp, q) -- (V.length xs)
-    q' <- fromMaybe (error "impos") <$> content q
-    xs' <- fromMaybe (error "impos") <$> content xProp
-    return (dist q', time q', Samp.mean xs, time xs')
-
 mixedLike nSamp std gen xsN thetaN betasN = do
   obs <- V.replicateM nSamp (resample xs gen)
   thetaSamp <- V.replicateM nSamp (resample theta gen)
@@ -381,7 +338,7 @@ mixedLike nSamp std gen xsN thetaN betasN = do
              (fromIntegral nSamp, V.map (V.! i) gradLikes, betaSamples))
         betasN)
   where
-    logSum v1 = V.sum . V.map exp . V.zipWith (+) v1
+    logSum v1 = log . V.sum . V.map exp . V.zipWith (+) v1
     -- obs = xsN
     xs = dist xsN
     theta = dist thetaN
@@ -419,12 +376,6 @@ mixedLikeScore nSamp std gen xsN thetaN betasN = do
     theta = dist thetaN
     betas = V.map dist betasN
 
-genNormal :: ST s (V.Vector Double)
-genNormal = do
-  gen <- create
-  xs <- V.replicateM 1000 (resample (normalDistr (5.0 :: Double) 3.0) gen)
-  return xs
-
 genMixture :: ST s (V.Vector Double)
 genMixture = do
   gen <- create
@@ -460,12 +411,12 @@ mixedFit xs =
                 , weight = fromIntegral nSamp
                 }))
     qThetas <-
-      resample (dirichlet (V.replicate nClusters 1)) gen1 >>= \startTh ->
+      -- resample (dirichlet (V.replicate nClusters 1)) gen1 >>= \startTh ->
         (known $
          ((defaultDirichlet priorTheta)
             { maxStep = globalMaxStep
             , delta = globalDelta -- 0.0000001
-            , dist = dirichlet startTh
+            , dist = dirichlet (V.replicate 2 1.0) -- startTh
             , weight = fromIntegral nSamp
             }))
     xProp <- known $ defaultObs xs
@@ -484,7 +435,7 @@ mixedFit xs =
        watch bPs $ \betas' ->
          with xP $ (\xs' -> do
             tP <- known =<< unsafeContent tP0
-            watch tP0 $
+            watch tP $
               (\theta' -> do
                  (upTh, _upB) <- mixedLikeScore nSamp 1.0 gen1 xs' theta' betas'
                  write tP upTh)
@@ -512,61 +463,14 @@ mixedFit xs =
 
 unsafeContent = (fromMaybe (error "impos") <$>) . content
 
-genDirichlet = do
-  gen <- create
-  xs <-
-    V.replicateM
-      1000
-      (resample (dirichlet (V.fromList [10.0, 20.0])) gen >>= \cat ->
-         MWCD.categorical (cat :: V.Vector Double) gen)
-  return (xs :: V.Vector Int)
-
-dirichletFit xs =
-  runST $ do
-    genG <- create
-    gen1 <- initialize =<< V.replicateM 256 (uniform genG)
-    let priorTheta = Diri (V.fromList [1.0, 1.0])
-    let nSamp = 100
-    let nClusters = 2
-    qTheta <-
-      -- resample (dirichlet (V.replicate nClusters 1)) gen1 >>= \startTh ->
-        (known $
-         ((defaultDirichlet priorTheta)
-            { maxStep = globalMaxStep
-            , delta = globalDelta -- 0.0000001
-            , dist = dirichlet (V.fromList [(1.0 :: Double), 1.0]) -- startTh
-            , weight = 1
-            }))
-    (\tP ->
-       watch tP $ \theta' -> do
-        upTh <- dirichletLike nSamp gen1 xs theta'
-        write tP upTh)
-      qTheta
-    thetaF <- unsafeContent qTheta
-    return (dist thetaF, time thetaF)
-
-dirichletLike nSamp gen xs diriQ =
-  V.replicateM nSamp (resample diri gen) >>= \samples ->
-    return $
-      gradientScore
-        diriQ
-        ( 1
-        , V.map
-          (\z ->
-             V.sum $ V.map (\i -> log (z V.! i)) xs)
-          samples
-        , samples)
-  where
-    diri = dist diriQ
-
 someFunc :: IO ()
 someFunc = do
   -- let xs = runST $ genNormal
   -- putStrLn (show $ normalFit xs)
-  -- let xs = runST $ genMixture
-  -- putStrLn $ unlines $ V.toList $ V.map show xs
-  -- putStrLn (show $ mixedFit xs)
-  let xs = runST $ genDirichlet
-  putStrLn (show $ dirichletFit xs)
+  let xs = runST $ genMixture
+  putStrLn $ unlines $ V.toList $ V.map show xs
+  putStrLn (show $ mixedFit xs)
+  -- let xs = runST $ genDirichlet
+  -- putStrLn (show $ dirichletFit xs)
 -- >>> someFunc
 --
