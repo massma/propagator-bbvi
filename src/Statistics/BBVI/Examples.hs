b502d235a1314a1ca8a5ef41f958823a8f4d6488
Adam Massmann <akm2203@columbia.edu>
2019-12-17 17:46:07 -0500

    updated to generalize to multidimensional gaussians

--- src/Statistics/BBVI/Examples.hs
+++ src/Statistics/BBVI/Examples.hs
@@ -46,9 +46,8 @@ globalMaxStep = 100000
 globalDelta :: Double
 globalDelta = 1e-16 -- 0.00001 --
 
-
 nDimension :: Int
-nDimension = 2
+nDimension = 1
 nTime :: Int
 nTime = 1000
 nState :: Int
@@ -58,72 +57,101 @@ nState = 3 -- 10
 --  mixture
 ------------------
 mixtureLike
-  :: (Dist b SampleVector, Differentiable c Double)
+  :: (Dist a SampleVector, Dist b SampleVector, Differentiable c Double)
   => Int
   -> Int
   -> Double
   -> GenST s
-  -> PropNode (Obs Double)
+  -> PropNode a
   -> PropNode b
-  -> V.Vector (PropNode c)
-  -> ST s (PropNode b, V.Vector (PropNode c))
+  -> V.Vector (V.Vector (PropNode c))
+  -> ST s (PropNode b, V.Vector (V.Vector (PropNode c)))
 mixtureLike nSamp nObs std gen xsN thetaN betasN = do
-  obs         <- V.replicateM nObs (resample xs gen)
+  obss        <- V.replicateM nObs (resample xss gen)
   thetaSamp   <- V.replicateM nSamp (resample theta gen)
-  betaSamples <- V.replicateM nSamp (epsilon (betas V.! 0) gen)
+  betaSamples <- V.replicateM nSamp (epsilon ((betass V.! 0) V.! 0) gen)
   let
     (likes, gradLikes) = V.unzip $ V.zipWith
       (\eps th ->
-        V.foldl1' (\(x1, y1) (x2, y2) -> (x1 + x2, V.zipWith (+) y1 y2)) $ V.map
-          (\x -> grad'
-            (\bs -> logSum
-              (V.map (auto . log) th)
-              (V.map (\mu -> diffableNormalLogProb mu (auto std) (auto x)) bs)
-            )
-            (V.map (\d -> transform d eps) betas)
-          )
-          (obs :: V.Vector Double)
+        V.foldl1'
+            (\(x1, y1) (x2, y2) -> (x1 + x2, V.zipWith (V.zipWith (+)) y1 y2))
+          $ V.map
+              (\obs ->
+                let
+                  (likes', gradss) = V.unzip $ V.zipWith
+                    (\ob betas -> grad'
+                      (\bs -> logSum
+                        (V.map (auto . log) th)
+                        (V.map
+                          (\mu -> diffableNormalLogProb mu (auto std) (auto ob))
+                          bs
+                        )
+                      )
+                      (V.map (\d -> transform d eps) betas)
+                    )
+                    obs
+                    betass
+                in  (V.sum likes', gradss)
+              )
+              (obss :: V.Vector (V.Vector Double))
       )
       betaSamples
       thetaSamp
   return
     $ ( gradientScore thetaN (1, likes, thetaSamp)
       , V.imap
-        (\i d -> gradientReparam d (1, V.map (V.! i) gradLikes, betaSamples))
+        (\dim v -> V.imap
+          (\c d -> gradientReparam
+            d
+            (1, V.map ((V.! c) . (V.! dim)) gradLikes, betaSamples)
+          )
+          v
+        )
+        betasN
+      )
+ where
+  xss    = dist xsN
+  theta  = dist thetaN
+  betass = V.map (V.map dist) betasN
+
+mixtureLikeScore nSamp nObs std gen xsN thetaN betasN = do
+  obss        <- V.replicateM nObs (resample xs gen)
+  thetaSamp   <- V.replicateM nSamp (resample theta gen)
+  betaSamples <- V.replicateM nSamp
+                              (V.mapM (V.mapM (\b -> resample b gen)) betass)
+  let likes = V.zipWith
+        (\bss th -> V.foldl1' (V.zipWith (+)) $ V.map
+          (\obs -> V.zipWith
+            (\ob bs -> logSum
+              (V.map log th)
+              (V.map (\mu -> logProb (normalDistr mu std) ob) bs)
+            )
+            obs
+            bss
+          )
+          (obss :: V.Vector (V.Vector Double))
+        )
+        betaSamples
+        thetaSamp
+  return
+    $ ( gradientScore thetaN (1, V.map V.sum likes, thetaSamp)
+      , V.imap
+        (\dim v -> V.imap
+          (\c d -> gradientScore
+            d
+            (1, V.map (V.! dim) likes, V.map ((V.! c) . (V.! dim)) betaSamples)
+          )
+          v
+        )
         betasN
       )
  where
     -- obs = xsN
-  xs    = dist xsN
-  theta = dist thetaN
-  betas = V.map dist betasN
+  xs     = dist xsN
+  theta  = dist thetaN
+  betass = V.map (V.map dist) betasN
 
-mixtureLikeScore nSamp nObs std gen xsN thetaN betasN = do
-  obs         <- V.replicateM nObs (resample xs gen)
-  thetaSamp   <- V.replicateM nSamp (resample theta gen)
-  betaSamples <- V.replicateM nSamp (V.mapM (\b -> resample b gen) betas)
-  let
-    likes = V.zipWith
-      (\bs th -> V.sum $ V.map
-        (\x -> logSum (V.map log th)
-                      (V.map (\mu -> logProb (normalDistr mu std) x) bs)
-        )
-        (obs :: V.Vector Double)
-      )
-      betaSamples
-      thetaSamp
-  return
-    $ ( gradientScore thetaN (1, likes, thetaSamp)
-      , V.imap (\i d -> gradientScore d (1, likes, V.map (V.! i) betaSamples))
-               betasN
-      )
- where
-    -- obs = xsN
-  xs    = dist xsN
-  theta = dist thetaN
-  betas = V.map dist betasN
-
-genMixture :: ST s (V.Vector Double)
+genMixture :: ST s (V.Vector (V.Vector Double))
 genMixture = do
   gen <- create
   let theta' = MWCD.categorical (V.replicate nState 1.0) gen
@@ -138,7 +166,7 @@ genMixture = do
           gen
         )
       )
-  xs <- V.replicateM nTime ((mixtures V.!) =<< theta')
+  xs <- V.replicateM nTime (V.replicateM nDimension . (mixtures V.!) =<< theta')
   return xs
 
 mixtureFit xs = runST $ do
@@ -149,18 +177,21 @@ mixtureFit xs = runST $ do
   let nSamp      = 10
   let nObs       = 100
   let localStep  = 20
-  qBetas <- known =<< V.generateM
-    nState
-    (\i -> do
-      mu <- resample priorBeta gen1
-      return
-        (defaultNormalDist { dist    = normalDistr mu (1.0 :: Double)
-                           , maxStep = globalMaxStep
-                           , delta   = globalDelta
-                           , prior   = priorBeta
-                           , weight  = 1
-                           }
-        )
+  qBetas <- known =<< V.replicateM
+    nDimension
+    (V.generateM
+      nState
+      (\i -> do
+        mu <- resample priorBeta gen1
+        return
+          (defaultNormalDist { dist    = normalDistr mu (1.0 :: Double)
+                             , maxStep = globalMaxStep
+                             , delta   = globalDelta
+                             , prior   = priorBeta
+                             , weight  = 1
+                             }
+          )
+      )
     )
   qThetas <-
     -- resample (dirichlet (V.replicate nClusters 1)) gen1 >>= \startTh ->
@@ -188,7 +219,9 @@ mixtureFit xs = runST $ do
   (\tP bPs0 xP -> watch tP $ \theta' ->
       with xP
         $ (\xs' -> do
-            bPs <- known =<< (V.map (initLocal localStep) <$> unsafeContent bPs0)
+            bPs <-
+              known
+                =<< (V.map (V.map (initLocal localStep)) <$> unsafeContent bPs0)
             watch bPs $ \betas' -> do
               (_upTh, upB) <- mixtureLikeScore nSamp
                                                nObs
@@ -229,10 +262,10 @@ mixtureFit xs = runST $ do
     xProp
   thetaF <- unsafeContent qThetas
   betaF  <- unsafeContent qBetas
-  let betaDists = V.map dist betaF
+  let betaDists = V.map (V.map dist) betaF
   return
     ( alphas (dist thetaF)
     , time thetaF
-    , V.map time betaF
-    , V.map (\d -> (mean d, stdDev d)) betaDists
+    , V.map (V.map time) betaF
+    , V.map (V.map (\d -> (mean d, stdDev d))) betaDists
     )
--- src/Statistics/BBVI/Observed.hs
+++ src/Statistics/BBVI/Observed.hs
@@ -9,12 +9,15 @@ where
 
 import qualified Data.Vector                   as V
 import           Statistics.BBVI.Class
-import           Statistics.BBVI.Propagator     ( PropNode(..) )
+import           Statistics.BBVI.Propagator     ( PropNode(..)
+                                                , SampleVector
+                                                , SampleDouble
+                                                )
 import           System.Random.MWC              ( uniformR )
 
 newtype Obs a = O (V.Vector a) deriving (Show, Eq, Ord, Read)
 
-defaultObs :: V.Vector Double -> PropNode (Obs Double)
+defaultObs :: V.Vector a -> PropNode (Obs a)
 defaultObs xs =
   (Node 1
         100000 -- global max step
@@ -26,12 +29,23 @@ defaultObs xs =
         (\_x1 _x2 -> (V.empty, V.empty))
   )
 
-instance Dist (Obs Double) Double where
-  resample (O d) gen = return . (d V.!) =<< uniformR (0, (V.length d - 1)) gen
-  logProb _d _x = 0
-  paramGradOfLogQ _d _x = V.empty
 
 instance DistUtil (Obs Double) where
   nParams _x = 0
   toParamVector _ = V.empty
   fromParamVector _ = O V.empty
+
+instance Dist (Obs Double) SampleDouble where
+  resample (O d) gen = return . (d V.!) =<< uniformR (0, (V.length d - 1)) gen
+  logProb _d _x = 0
+  paramGradOfLogQ _d _x = V.empty
+
+instance DistUtil (Obs SampleVector) where
+  nParams _x = 0
+  toParamVector _ = V.empty
+  fromParamVector _ = O V.empty
+
+instance Dist (Obs SampleVector) SampleVector where
+  resample (O d) gen = return . (d V.!) =<< uniformR (0, (V.length d - 1)) gen
+  logProb _d _x = 0
+  paramGradOfLogQ _d _x = V.empty
